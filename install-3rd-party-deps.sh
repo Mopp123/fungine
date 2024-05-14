47a12267207a85824e00f728a7f0b91b28ea18bd
mpfff <keahonen@gmail.com>
2024-05-15 02:11:20 +0300

    Updated install script

--- install-3rd-party-deps.sh
+++ install-3rd-party-deps.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # NOTE: This should be run with sudo
+apt-get update
 
 apt-get install build-essential libxmu-dev libxi-dev libgl-dev
 
 # For windowing on linux
 apt-get install libwayland-dev libxkbcommon-dev xorg-dev
+
+# For building freetype and harfbuzz
+apt-get install automake libtool autoconf libfreetype6-dev libglib2.0-dev libcairo2-dev meson pkg-config gtk-doc-tools
